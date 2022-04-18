932da55b3974c273d274fc70b51c52c2f008bf7f
vj <592909057@qq.com>
2022-04-18 17:08:34 +0800

    change price views

--- pricecheck.py
+++ pricecheck.py
@@ -52,7 +52,7 @@ class PriceCheck(object):
             for i in response["orders"]:
                 self.order_count += 1
                 number = i['number']
-                print("%s order:%s" % (nation, number))
+                # print("%s order:%s" % (nation, number))
                 self.price_check(nation, number)
         except requests.exceptions.HTTPError as e:
             print("error occurred: ", e)
@@ -95,55 +95,55 @@ class PriceCheck(object):
         tax_price = float('%.2f' % tax_price)  # 精度转换
 
         if total_price == r['amount_total'] and tax_price == r['tax_amount_total']:
-            print('pass')
+            # print('pass')
             return True
         else:
-            print("%s is not equal to %s" % (total_price, r['amount_total']))
+            print("%s-%s: %s is not equal to %s" % (nation, number, total_price, r['amount_total']))
             self.error_count += 1
             return False
 
-    def check_test(self, nation, number):
-        if nation == 'au':
-            u = "https://knight-test.castlery.com.au/spree/api/orders/" + number + "/oms"
-            header = {'X-Spree-Token': '017060db65725b67d25062974e0fdae9533a05b8b3eee1f2'}
-        elif nation == 'us':
-            u = "https://knight-test.castlery.co/spree/api/orders/" + number + "/oms"
-            header = {'X-Spree-Token': 'be6c6aa6aa0f835badb041d6be405cadb52bd3097a1d611b'}
-        elif nation == 'sg':
-            u = "https://knight-test.castlery.sg/spree/api/orders/" + number + "/oms"
-            header = {'X-Spree-Token': 'f35b35a4f6c286c2b855ff8df12c113ee0e9a37e532ece41'}
-        else:
-            print('wrong nation')
-            return False
-
-        # try一下看请求是否正确返回
-        try:
-            requests.get(url=u, headers=header).raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            print("404:", e)
-            return False
-        else:
-            r = requests.get(url=u, headers=header).json()
-
-        # 价格计算
-        item_price = r['shipping_fee'] - r['shipping_fee_discount']
-        tax_price = r['shipping_fee_tax']
-
-        for item in r['lines']:
-            item_price += item['unit_price'] * item['quantity'] - item['line_discount']
-            tax_price += item['line_tax']
-
-        total_price = item_price + tax_price
-        total_price = float('%.2f' % total_price)  # 精度转换
-        tax_price = float('%.2f' % tax_price)  # 精度转换
-
-        print(total_price)
-        if total_price == r['amount_total'] and tax_price == r['tax_amount_total']:
-            print('pass')
-            return True
-        else:
-            print("%s is not equal to %s" % (total_price, r['amount_total']))
-            return False
+    # def check_test(self, nation, number):
+    #     if nation == 'au':
+    #         u = "https://knight-test.castlery.com.au/spree/api/orders/" + number + "/oms"
+    #         header = {'X-Spree-Token': '017060db65725b67d25062974e0fdae9533a05b8b3eee1f2'}
+    #     elif nation == 'us':
+    #         u = "https://knight-test.castlery.co/spree/api/orders/" + number + "/oms"
+    #         header = {'X-Spree-Token': 'be6c6aa6aa0f835badb041d6be405cadb52bd3097a1d611b'}
+    #     elif nation == 'sg':
+    #         u = "https://knight-test.castlery.sg/spree/api/orders/" + number + "/oms"
+    #         header = {'X-Spree-Token': 'f35b35a4f6c286c2b855ff8df12c113ee0e9a37e532ece41'}
+    #     else:
+    #         print('wrong nation')
+    #         return False
+    #
+    #     # try一下看请求是否正确返回
+    #     try:
+    #         requests.get(url=u, headers=header).raise_for_status()
+    #     except requests.exceptions.HTTPError as e:
+    #         print("404:", e)
+    #         return False
+    #     else:
+    #         r = requests.get(url=u, headers=header).json()
+    #
+    #     # 价格计算
+    #     item_price = r['shipping_fee'] - r['shipping_fee_discount']
+    #     tax_price = r['shipping_fee_tax']
+    #
+    #     for item in r['lines']:
+    #         item_price += item['unit_price'] * item['quantity'] - item['line_discount']
+    #         tax_price += item['line_tax']
+    #
+    #     total_price = item_price + tax_price
+    #     total_price = float('%.2f' % total_price)  # 精度转换
+    #     tax_price = float('%.2f' % tax_price)  # 精度转换
+    #
+    #     print(total_price)
+    #     if total_price == r['amount_total'] and tax_price == r['tax_amount_total']:
+    #         print('pass')
+    #         return True
+    #     else:
+    #         print("%s is not equal to %s" % (total_price, r['amount_total']))
+    #         return False
 
 
 pc = PriceCheck()
